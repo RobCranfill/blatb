b5d63e61ed374e7d9c373b9f13bc97a50c27e2a0
Rob Cranfill <robcranfill@gmail.com>
2020-12-30 17:30:53 -0800

    Fixed script

--- findminmax.sh
+++ findminmax.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-awk '{print $4}' blatb-data.log | sort | head -1
-awk '{print $4}' blatb-data.log | sort | tail -1
+awk '{print $4}' blatb-data.log | sort -n | head -1
+awk '{print $4}' blatb-data.log | sort -n | tail -1
