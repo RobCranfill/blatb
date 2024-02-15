0e147e5f8ddbb2257f8ce570e6ed90286499a64c
Rob Cranfill <robcranfill@gmail.com>
2024-02-15 09:22:41 -0800

    Show labels on output

--- findminmax.sh
+++ findminmax.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # find the min and max of the lux values
+
+echo -n "Min: "
 awk '{print $4}' blatb-data.log | sort -n | head -1
+
+echo -n "Max: "
 awk '{print $4}' blatb-data.log | sort -n | tail -1
